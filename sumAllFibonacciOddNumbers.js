985fea9520369d8fb24f479bc43a4049e93196d0
Paulo Santiago <pauloecsantiago@gmail.com>
2023-03-11 16:44:23 +0000

    Sum of all Fibonacci odds

---
+++ sumAllFibonacciOddNumbers.js
@@ -0,0 +1,112 @@
+/* Sum All Odd Fibonacci Numbers
+
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 0 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first seven numbers of the Fibonacci sequence are 0, 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+ */
+
+function sumFibs(num) {
+  let a = 1;
+  let b = 1;
+  let sum = 1;
+
+  while (b <= num) {
+    if (b % 2 == 1) {
+      sum += b;
+    }
+    let temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return sum;
+}
+
+/* Tests */
+
+console.log(sumFibs(1)); // should return a number.
+console.log(sumFibs(1000)); // should return 1785.
+console.log(sumFibs(4000000)); // should return 4613732.
+console.log(sumFibs(4)); // should return 5.
+console.log(sumFibs(75024)); // should return 60696.
+console.log(sumFibs(75025)); // should return 135721.
+
+/* Solution 1
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+// test here
+sumFibs(4);
+
+
+
+/* Solution 2 
+function sumFibs(num) {
+  // Perform checks for the validity of the input
+  if (num <= 0) return 0;
+
+  // Create an array of fib numbers till num
+  const arrFib = [1, 1];
+  let nextFib = 0;
+
+  // We put the new Fibonacci numbers to the front so we
+  // don't need to calculate the length of the array on each
+  // iteration
+  while ((nextFib = arrFib[0] + arrFib[1]) <= num) {
+    arrFib.unshift(nextFib);
+  }
+
+  // We filter the array to get the odd numbers and reduce them to get their sum.
+  return arrFib.filter(x => x % 2 != 0).reduce((a, b) => a + b);
+}
+
+// test here
+sumFibs(4);
+
+
+
+
+
+
+
+*/
+
+/* Solution 3
+
+function sumFibs(num) {
+  // Every third Fibbonaci number is even
+  //   and the rest are odd
+  let f0 = 0;
+  let f1 = 1;
+  let f2 = 1;
+
+  // Generate triples until num is reached
+  let sum = 0;
+  while (f1 <= num) {
+    // Update sum
+    sum += f1;
+    if (f2 <= num) sum += f2;
+
+    // Compute next three Fibonacci numbers
+    [f0, f1] = [f1 + f2, f2 + (f1 + f2)];
+    f2 = f0 + f1;
+  }
+
+  return sum;
+}
+
+
+*/
